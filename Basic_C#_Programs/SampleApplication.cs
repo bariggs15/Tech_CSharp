e43ebf859536dff60cbc5d435add3533086338b2
bariggs15 <bariggs15@gmail.com>
2021-02-15 15:43:52 -0600

    Add basic programs

---
+++ Basic_C#_Programs/SampleApplication.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleApplication.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
