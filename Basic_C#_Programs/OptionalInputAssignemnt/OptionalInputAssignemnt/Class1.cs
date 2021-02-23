37c2397e187344862e0be46e219d0624ce30f326
bariggs15 <bariggs15@gmail.com>
2021-02-22 22:31:13 -0600

    optional class assignment

---
+++ Basic_C#_Programs/OptionalInputAssignemnt/OptionalInputAssignemnt/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalInputAssignemnt
+{
+    public class Num1
+    {
+        public int Add1(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ Basic_C#_Programs/OptionalInputAssignemnt/OptionalInputAssignemnt/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalInputAssignemnt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("input a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("input another number if you want, if not type 0");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Num1 number1 = new Num1();
+
+            int result = number1.Add1(num1, num2);
+
+            Console.WriteLine("if you add your two numbers together you get: " + result);
+            Console.ReadLine();
+        }
+    }
+}
