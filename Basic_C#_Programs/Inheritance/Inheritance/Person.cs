8d360bf8ae0db753b25f8cbda87e6b61b1ddd3fe
bariggs15 <bariggs15@gmail.com>
2021-02-24 10:15:07 -0600

    editted inheritance project

---
+++ Basic_C#_Programs/Inheritance/Inheritance/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Person
+    {
+        public void SayName(string x, string y)
+        {
+            string result = x + " " + y;
+            Console.WriteLine("Name: " + result);
+
+        }
+        //public string FullName(string x, string y)
+        //{
+        //    string result = x + " " +  y;
+        //    return result; 
+
+        //}
+
+       
+    }
+}
---
+++ Basic_C#_Programs/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name?");
+            string fname = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            string lname = Console.ReadLine();
+
+            Person person = new Person();
+            Employee employee1 = new Employee();
+            
+
+            person.SayName(fname, lname);
+            employee1.SayName("Sample", "Student");
+            
+            Console.ReadLine();
+        }
+    }
+}
+            //string result1 = person.FullName(fname,lname);
+
+            //Console.WriteLine("Your full name is " + result1);
