4bd135716c4976b878e84943f24ae01c6c6cb58d
bariggs15 <bariggs15@gmail.com>
2021-02-24 17:03:25 -0600

    editted 21 game

--- Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
@@ -10,61 +10,65 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            //Game game = new Game();
+          
             TwentyOneGame game = new TwentyOneGame();
             game.Players = new List<string>() { "Austin", "Bob", "Joe" };
             game.ListPlayers();
-            game.Play();
+            //game.Play();
             Console.ReadLine();
-
-            //Deck deck = new Deck();
-            //deck.Shuffle(3);
-
-        //    foreach(Card card in deck.Cards)
-        //    {
-        //        Console.WriteLine(card.face + " of " + card.suit);
-        //    }
-        //    Console.WriteLine(deck.Cards.Count);
-        //    Console.ReadLine();
-        //
-        //    Deck deck = new Deck();
-        //    //int timesShuffled = 0;
-        //    //deck.Shuffle(/*deck: deck, out timesShuffled, times: */3);
-        //    //deck = Shuffle(deck, 3);
-        //    deck.Shuffle(3);
-
-
-        //    foreach (Card card in deck.Cards)
-        //    {
-        //        Console.WriteLine(card.face + " of " + card.suit);
-            
-        //    }
-        //    Console.WriteLine(deck.Cards.Count);
-        //    //Console.WriteLine("times shuffled {0}", timesShuffled);
-        //    Console.ReadLine();
-        //
-
- 
-        //public static Deck Shuffle(Deck deck, int times)
-        //{
-        //    for (int i = 0; i < times; i++) 
-        //    {
-        //        deck = Shuffle(deck);
-        //    }
-        //    return deck;
+      
         }
     }
 }
 
+////Game game = new Game();
+//List<Game> games = new List<Game>();
+//Game game = new TwentyOneGame();
+//games.Add(game);
+//Deck deck = new Deck();
+//deck.Shuffle(3);
+
+//    foreach(Card card in deck.Cards)
+//    {
+//        Console.WriteLine(card.face + " of " + card.suit);
+//    }
+//    Console.WriteLine(deck.Cards.Count);
+//    Console.ReadLine();
+//
+//    Deck deck = new Deck();
+//    //int timesShuffled = 0;
+//    //deck.Shuffle(/*deck: deck, out timesShuffled, times: */3);
+//    //deck = Shuffle(deck, 3);
+//    deck.Shuffle(3);
+
+
+//    foreach (Card card in deck.Cards)
+//    {
+//        Console.WriteLine(card.face + " of " + card.suit);
+
+//    }
+//    Console.WriteLine(deck.Cards.Count);
+//    //Console.WriteLine("times shuffled {0}", timesShuffled);
+//    Console.ReadLine();
+//
+
+
+//public static Deck Shuffle(Deck deck, int times)
+//{
+//    for (int i = 0; i < times; i++) 
+//    {
+//        deck = Shuffle(deck);
+//    }
+//    return deck;
 
 //deck.Cards = new List<Card>();
 
 
 
-            //Card cardOne = new Card();
-            //cardOne.face = "Queen";
-            //cardOne.suit = "Spades";
+//Card cardOne = new Card();
+//cardOne.face = "Queen";
+//cardOne.suit = "Spades";
 
-            //deck.Cards.Add(cardOne);
+//deck.Cards.Add(cardOne);
 
-            //Console.WriteLine(cardOne.face + " of " + cardOne.suit);
+//Console.WriteLine(cardOne.face + " of " + cardOne.suit);
