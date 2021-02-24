3c9280c62497a79c4c7c8cb240f4316f4c49db9d
bariggs15 <bariggs15@gmail.com>
2021-02-23 19:29:54 -0600

    void methods

---
+++ Basic_C#_Programs/VoidMethods/VoidMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("input a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("input another number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Void void1 = new Void();
+
+            int result1 = void1.Squared(num1);
+            int result2 = void1.squared(num2);
+
+            Console.WriteLine("your number square is " + result1);
+            Console.WriteLine("your second number was: " + result2);
+            Console.ReadLine();
+        }
+
+    }
+}
+
---
+++ Basic_C#_Programs/VoidMethods/VoidMethods/Void.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethods
+{
+    class Void
+    {
+        public void int Squared(int x, int y)
+        {
+            return x ^ 2;
+            
+        }
+    }
+}
