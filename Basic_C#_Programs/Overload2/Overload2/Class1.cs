3857ce91598f7a2589d833a510969725e893e03a
bariggs15 <bariggs15@gmail.com>
2021-02-25 15:21:31 -0600

    overload2 project

---
+++ Basic_C#_Programs/Overload2/Overload2/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overload2
+{
+    public class EmployeeID
+    {
+        public  string Employee1 { get; set; }
+        public string Employee2 { get; set; }
+
+        public void EmployeeCheck(string x, string y)
+        {
+            if (x == y)
+            {
+                throw new ArgumentException("Cannot have same ID#");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("the first employee is " + x + "\n the second employee is " + y);
+                Console.ReadLine();
+            }
+        }
+        
+    }
+   
+}
+
---
+++ Basic_C#_Programs/Overload2/Overload2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overload2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeID employee = new EmployeeID();
+
+            employee.Employee1 = "Jordans";
+            employee.Employee2 = "Jordan";
+            employee.EmployeeCheck(employee.Employee1, employee.Employee2);
+
+           
+
+            
+            Console.ReadLine();
+        }
+    }
+}
