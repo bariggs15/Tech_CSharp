755f0eca4fd0e756d1c42cdfbe067e9a6588db5f
bariggs15 <bariggs15@gmail.com>
2021-02-25 22:24:07 -0600

    enum assignment

--- Basic_C#_Programs/Enums/Enums/Program.cs
+++ Basic_C#_Programs/Enums/Enums/Program.cs
@@ -11,24 +11,35 @@ namespace Enums
         static void Main(string[] args)
         {
            
-                Console.WriteLine("Please enter current day of week: ");
-                string day1 = Console.ReadLine();
+            Console.WriteLine("Please enter current day of week: ");
+            string day1 = Console.ReadLine();
+
+            try { 
             
-            try
-            {   
-                foreach (string day in Enum.GetNames(typeof(DaysOfWeek)))
-                {
-                    Console.WriteLine("The corresponding value is: {0} = {1:D}", day, Enum.Parse(typeof(DaysOfWeek), day));
-                }
-                Console.ReadLine();
-                //DayOfWeek day = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), day1);
-                //Console.WriteLine();
-                //Console.ReadLine();
+            Console.WriteLine("The corresponding value is: {0} = {1:D} ", day1, Enum.Parse(typeof(DaysOfWeek), day1));
+            Console.ReadLine();
             }
-            catch  (ArgumentException e)
+            catch (Exception e)
             {
-                Console.WriteLine("Please enter actual day of week", e);
+                Console.WriteLine("Please enter actual day of week", e.Message);
             }
+            Console.ReadLine();
+
+            //try
+            //{   
+            //    foreach (string day in Enum.GetNames(typeof(DaysOfWeek)))
+            //    {
+            //        Console.WriteLine("The corresponding value is: {0} = {1:D}", day, Enum.Parse(typeof(DaysOfWeek), day));
+            //    }
+            //    Console.ReadLine();
+            //    //DayOfWeek day = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), day1);
+            //    //Console.WriteLine();
+            //    //Console.ReadLine();
+            //}
+            //catch  (ArgumentException e)
+            //{
+            //    Console.WriteLine("Please enter actual day of week", e);
+            //}
         }
     }
 }
