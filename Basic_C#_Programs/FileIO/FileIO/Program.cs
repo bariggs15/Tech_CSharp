fff1b921d2af462264550d4c79f25148612c4428
bariggs15 <bariggs15@gmail.com>
2021-02-28 08:56:03 -0600

    edit logs

--- Basic_C#_Programs/FileIO/FileIO/Program.cs
+++ Basic_C#_Programs/FileIO/FileIO/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace FileIO
 {
@@ -10,6 +11,9 @@ namespace FileIO
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("type a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            File.WriteAllText(@"C:\Users\arigg\Logs\log.txt", Convert.ToString(num1));
         }
     }
 }
