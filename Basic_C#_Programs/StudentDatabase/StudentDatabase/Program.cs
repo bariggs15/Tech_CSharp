9f44f197f0fbffb99b50dae8aae68eb8dcb419d2
bariggs15 <bariggs15@gmail.com>
2021-03-11 00:58:35 -0600

    edit student database

---
+++ Basic_C#_Programs/StudentDatabase/StudentDatabase/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace StudentDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new  PostStudent())
+            {
+                Console.WriteLine("What is your first name?");
+                var FirstName = Console.ReadLine();
+
+                var student1 = new student { FirstName = FirstName };
+                db.PostStudents.Add(student1);
+                db.SaveChanges();
+
+                var query = from b in db.PostStudents
+                            orderby b.FirstName
+                            select b;
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.FirstName);
+                }
+                            
+            }
+        }
+    }
+}
---
+++ Basic_C#_Programs/StudentDatabase/StudentDatabase/student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Entity;
+
+namespace StudentDatabase
+{
+    class student
+    {
+        public string FirstName { get; set; }
+        public virtual List<PostStudent> PostStudents{ get; set;}
+    }
+
+    public class PostStudent: DbStudent
+    {
+        public DbSet<PostStudent> PostStudents { get; set; }
+    }
+
+    public class DbStudent
+    {
+    }
+}
